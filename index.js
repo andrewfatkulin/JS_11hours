7dea8f96f0c484c5033af07dc3d30a0b5691dc0d
andrewfatkulin <102696022+andrewfatkulin@users.noreply.github.com>
2023-01-31 11:32:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,120 @@
+// let a;
+// a = true;
+// console.log(a);
+
+// let b;
+// console.log(b);
+
+// const c = 10;
+// console.log(c);
+
+// let myName;
+// console.log(myName);
+// myName = 'Andrew';
+// console.log(myName);
+
+// console.log(console.log(10));
+
+// const myName = 'Andrew';
+// myName = 'Oksana'; //Error
+// console.log(myName);
+
+// const objectA = {
+//     a: 10,
+//     b: true,
+//     c: undefined,
+//     d: 'Andrew',
+// }
+// const copyOfA = objectA;
+// copyOfA.a = 20;
+// copyOfA.e = null;
+// console.log(copyOfA);
+
+// let a = 10;
+// a = true;
+// a = 'Andrew';
+// a = undefined;
+// console.log(a);
+
+// function a () {
+//     console.log('Hey there');
+// }
+// a ();
+// // a = 10;
+// // console.log(a);
+// // a ();
+
+// const a = () => {
+//     console.log('Hey there');
+// }
+// a ();
+// // a = 10;
+// // a ();
+
+// const myCity =  {
+//     city: 'Ivanovo',
+//     popular: true,
+//     country: 'Russia',
+// }
+// console.log(myCity);
+// console.log(myCity.city);
+// console.log(myCity.country);
+// myCity.city = 'Kohma';
+// console.log(myCity);
+
+// const myCity = {
+//     city: 'Ivanovo'
+// }
+// console.log(myCity);
+// myCity.popular = true; // добавление свойств
+// console.log(myCity);
+// myCity.country = 'Russia'; // добавление свойств
+// console.log(myCity);
+
+// delete myCity.popular; // удаление свойств
+// console.log(myCity);
+
+// myCity = {
+//     city: 'Ivanovo',
+// }
+// console.log(myCity);
+// myCity['popular'] = true;
+// console.log(myCity);
+// const countryPropertyName = 'country';
+// myCity [countryPropertyName] = 'Russia';
+// console.log(myCity);
+
+// const myCity = {
+//     city: 'Ivanovo',
+//     info: {
+//         isPopular: true,
+//         country: 'Russia',
+//     }
+// }
+// console.log(myCity);
+// console.log(myCity.info.country);
+// delete myCity.info.isPopular;
+// console.log(myCity);
+// delete myCity.info ['country'];
+// console.log(myCity);
+// delete myCity.info;
+// console.log(myCity);
+
+const name = 'Andrew';
+const postsQty = 35;
+
+// const userProfile = {
+//     name: name,
+//     postsQty: postsQty,
+//     hasSignedAgreement: false,
+// }
+// console.log(userProfile);
+
+const userProfile = { // таже запись, что выше, но в сокращенном формате
+    name,
+    postsQty,
+    hasSignedAgreement: false,
+}
+console.log(userProfile);
+
+
