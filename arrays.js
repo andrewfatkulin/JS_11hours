db04bb2478370fc60022cf6599cb0e096ca61f76
andrewfatkulin <102696022+andrewfatkulin@users.noreply.github.com>
2023-03-06 22:58:25 +0300

    Add files via upload

--- arrays.js
+++ arrays.js
@@ -1,5 +1,9 @@
 // const myArray = [1,2,3];
 // console.log(myArray);
 
-const myArray2 = new Array (1, 2, 3);
-console.log(myArray2);
+// const myArray2 = new Array (1, 2, 3);
+// console.log(myArray2);
+
+const myArray3 = [1, true, 'Andrew'];
+console.log(myArray3);
+console.log(myArray3.length);
