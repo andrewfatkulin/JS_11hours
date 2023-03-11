eb2be298173901c9c8435a0d350263b1a21a70ee
andrewfatkulin <102696022+andrewfatkulin@users.noreply.github.com>
2023-03-11 23:14:42 +0300

    Add files via upload

--- destructuring assignment.js
+++ destructuring assignment.js
@@ -1,5 +1,4 @@
 // Деструктуризация объектов
-
 // const userProfile = {
 //     name: 'Andrew',
 //     commentsQty: 23,
@@ -7,13 +6,25 @@
 // }
 // const {name, commentsQty} = userProfile
 // const {hasSignedAgreement} = userProfile
-
 // console.log(name);
 // console.log(commentsQty);
 
 // Деструктуризация массивов
+// const fruits = ['Apple', 'Banana']
+// const [fruitOne, fruitTwo] = fruits
+// console.log(fruitOne);
+// console.log(fruitTwo);
 
-const fruits = ['Apple', 'Banana']
-const [fruitOne, fruitTwo] = fruits
-console.log(fruitOne);
-console.log(fruitTwo);
+// Деструктуризация в функциях
+// const userProfile = {
+//     name: 'Andrew',
+//     commentsQty: 35,
+//     hasSignedAgreement: false
+// }
+// const userInfo = ({name, commentsQty}) => {
+//     if (!commentsQty){
+//         return `User ${name} has no comments`
+//     }
+//     return `User ${name} has ${commentsQty} comments`
+// }
+// console.log(userInfo (userProfile));
