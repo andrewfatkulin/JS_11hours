ff9bd6076515b3ef46beab187debe5cf12e07674
andrewfatkulin <102696022+andrewfatkulin@users.noreply.github.com>
2023-03-22 13:07:57 +0300

    Add files via upload

--- conditionalStatement.js
+++ conditionalStatement.js
@@ -1,12 +1,52 @@
+// Условные инструкции
+
+// Инструкция if:
 // let val = 10
 // if (val > 5) {
 //     val += 20
 // }
 // console.log(val);
 
-const person = {
-    age: 20
-}
-if (person.name) {
-    console.log('Имя не указано');
-}
+// Инструкция с оператором отрицания !:
+// const person = {
+//     age: 20
+// }
+// if (person.name) {
+//     console.log('Имя не указано');
+// }
+
+// Инструкция if-else:
+// let val = 10
+// if (val < 5) {
+//     val += 20
+// } else {
+//     val -= 20
+// }
+// console.log(val);
+
+// Инструкция if else if:
+// const age = 25
+// if (age > 18) {
+//     console.log('Is adult');
+// } else if (age >= 12) {
+//     console.log('Is teenager');
+// } else {
+//     console.log('Is child');
+// }
+
+// Тот же самый пример, что и выше, используя только инструкции if:
+// const age = 25
+
+// if (age >= 18) {
+//     console.log('Is adult');
+// }
+
+// if (age >= 12 && age < 18) {
+//     console.log('Is teenager');
+// }
+
+// if (age < 12) {
+//     console.log('Is child');
+// }
+
+// Использование if в функциях:
