5acbdb571bd1c512a40e899ae3948b893865e451
andrewfatkulin <102696022+andrewfatkulin@users.noreply.github.com>
2023-03-27 13:57:57 +0300

    Add files via upload

---
+++ сonditionalOperator.js
@@ -0,0 +1,26 @@
+// Example 1:
+// const value = 25
+// value
+// ? console.log('Условие истинно')
+// : console.log('Условие ложно')
+
+// Example 2:
+// const value1 = 11
+// const value2 = 25
+// value1 && value2
+// ? myFunction1 (value1, value2) // В данном примере функции не задаются
+// : myFunction2 ()
+
+// Example 3:
+// let value = 11
+// console.log(value >=0 ? value : -value);
+
+// value = -5
+// const res = value >=0 ? value : -value // Присвоение результата переменной
+// console.log(res);
+
+// value = -5
+// const res = value >=0 //тернарный оператор в 3 строки
+// ? value
+// : -value
+// console.log(res);
