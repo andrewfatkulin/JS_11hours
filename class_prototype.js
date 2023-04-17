0e6f379b36682eff644dc02836a0e9da47759bcb
andrewfatkulin <102696022+andrewfatkulin@users.noreply.github.com>
2023-04-17 21:33:32 +0300

    Add files via upload

--- class_prototype.js
+++ class_prototype.js
@@ -7,5 +7,7 @@ class Comment {
         this.votesQty += 1
     }
 }
-const firstComment = new Comment ('First comment')
+const firstComment = new Comment ('First comment') // создание нового экземпляра класса
 console.log(firstComment);
+console.log (firstComment instanceof Comment) // проверка принадлежности
+console.log (firstComment instanceof Object) // проверка принадлежности
