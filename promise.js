59691e9171f82e54c35fdd226ce224bf274185f0
andrewfatkulin <102696022+andrewfatkulin@users.noreply.github.com>
2023-04-27 21:49:29 +0300

    Add files via upload

--- promise.js
+++ promise.js
@@ -1,7 +1,39 @@
-fetch ('https://jsonplaceholder.typicode.com/todos')
-.then (response => {
-    console.log(response);
-    return response.json()
-})
-.then (json => console.log(json))
-.catch (error => console.error(error))
+// fetch ('https://jsonplaceholder.typicode.com/todos')
+// .then (response => {
+//     console.log(response);
+//     return response.json()
+// })
+// .then (json => console.log(json))
+// .catch (error => console.error(error))
+
+// fetch ('https://jsonplaceholder.typicode.com/todos/1') // добавляем /1 , чтобы получить только один объект с id 1
+// .then (response => {
+//     console.log(response);
+//     return response.json()
+// })
+// .then (json => console.log(json))
+// .catch (error => console.error(error))
+
+// fetch ('https://jsonplaceholder.typicode.com/todos')
+// .then (response => response.json()) // неявный возврат метода json
+// .then (json => console.log(json))
+// .catch (error => console.error(error))
+
+// fetch ('https://jsonplaceholder.typicode.com/todos')
+// .then (response => response.json()) // неявный возврат метода json
+// .then (json => console.log(json))
+// .catch (error => console.log(error.message)) // вывод сообщения о конкретной ошибке
+
+// Пример Промиса
+
+const getData = (url) =>
+    new Promise ((resolve, reject) =>
+    fetch (url)
+    .then (response => response.json())
+    .then (json => resolve (json))
+    .catch (error => reject (error))
+)
+
+getData ('https://jsonplaceholder.typicode.com/todos')
+.then (data => console.log (data))
+.catch (error => console.log (error.message))
