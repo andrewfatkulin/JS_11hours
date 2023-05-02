c9e0d2874fbf0489488fd4ef29f23fec6911c1f6
andrewfatkulin <102696022+andrewfatkulin@users.noreply.github.com>
2023-05-02 23:44:14 +0300

    Add files via upload

---
+++ asyncAwait.js
@@ -0,0 +1,15 @@
+// const asyncFn = async () => {
+//     return 'Success!'
+// }
+// console.log(asyncFn());
+
+// const asyncFn = async () => {
+//     return 10
+// }
+// console.log(asyncFn());
+
+// const asyncFn = async () => {
+//     return 'Success!'
+// }
+// asyncFn();
+// .then (value => console.log(value);) 
