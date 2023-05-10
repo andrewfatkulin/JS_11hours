59c664e6ab60a9c0aa8685ef971f28111e53a559
andrewfatkulin <102696022+andrewfatkulin@users.noreply.github.com>
2023-05-10 13:26:01 +0300

    Add files via upload

--- asyncAwait.js
+++ asyncAwait.js
@@ -13,3 +13,49 @@
 // }
 // asyncFn();
 // .then (value => console.log(value);) 
+
+// Асинхронная функция возвращает ошибку:
+
+// const asyncFn = async () => {
+//     throw new Error ('There was an error')
+// }
+// asyncFn();
+
+// Обравотка ошибки с помощью методов then и catch
+
+// const asyncFn = async () => {
+//     throw new Error ('There was an error')
+// }
+// asyncFn();
+// .then (value => console.log(value))
+// .catch (error => console.log(error.message))
+
+// Ожидание результата await
+
+// const timerPromise = () => 
+//     new Promise ((resolve, reject) => 
+//     setTimeout (() => resolve (), 2000))
+
+// const asyncFn = async () => {
+//     console.log('Time starts');
+//     await timerPromise (),
+//     console.log('Time ended');
+// }
+// console.log(asyncFn());
+
+// Тот же самый пример, но с отслеживанием выполнения промиса:
+
+const timerPromise = () => 
+    new Promise ((resolve, reject) => 
+    setTimeout (() => resolve (), 2000))
+
+const asyncFn = async () => {
+    console.log('Time starts');
+    const startTime = performance.now()
+    await timerPromise ()
+    const endTime = performance.now()
+    console.log('Time ended', endTime - startTime);
+}
+console.log(asyncFn());
+
+// Переход с промисов на async/await:
